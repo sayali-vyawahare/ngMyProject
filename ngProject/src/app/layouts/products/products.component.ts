9aaf07d713c168409fd16c939f8dd66555c40035
sayali-vyawahare <sayali.v246@gmail.com>
2020-09-21 18:08:06 +0530

    changes for if

--- ngProject/src/app/layouts/products/products.component.ts
+++ ngProject/src/app/layouts/products/products.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  title = 'Welcome to product section';
+  mobParts = [
+    {
+      prod_id: 10100,
+      prod_name: 'Iphone',
+      prodInstock: 150,
+      prodPrice: 118000,
+    },
+    {
+      prod_id: 10200,
+      prod_name: 'Samsung',
+      prodInstock: 150,
+      prodPrice: 18000,
+    },
+    {
+      prod_id: 10300,
+      prod_name: 'Nokia',
+      prodInstock: 150,
+      prodPrice: 25000,
+    },
+  ];
 }
