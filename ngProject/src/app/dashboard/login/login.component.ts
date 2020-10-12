b9f8e009b5e56ab1c36705a0f1619f74690489c3
sayali-vyawahare <sayali.v246@gmail.com>
2020-10-12 18:28:20 +0530

    auth

--- ngProject/src/app/dashboard/login/login.component.ts
+++ ngProject/src/app/dashboard/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class LoginComponent implements OnInit {
+  msg: String = '';
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
   login(uname, pass) {
     console.log('LoginComponent -> login -> pass', pass);
     console.log('LoginComponent -> login -> uname', uname);
+
+    if (uname === 'admin' && pass === 'admin'){
+      localStorage.setItem('admin', 'admin');
+      this.router.navigate(['/dash']);
+    }
+    this.msg = "Please try again..!";
 
 }
 }
--- ngProject/src/app/modules/app-routing/app-routing.module.ts
+++ ngProject/src/app/modules/app-routing/app-routing.module.ts
@@ -11,6 +11,8 @@ import { MotoComponent } from 'src/app/products/mobiles/moto/moto.component';
 import { SamsungComponent } from 'src/app/products/mobiles/samsung/samsung.component';
 import { MobMenuComponent } from 'src/app/products/mobiles/mob-menu/mob-menu.component';
 import { LoginComponent } from "./../../dashboard/login/login.component";
+import { DashComponent } from 'src/app/dashboard/dash/dash.component';
+import { AuthGuard } from 'src/app/guard/auth.guard';
 
 
 const appRoutes: Routes = [
@@ -32,6 +34,8 @@ const appRoutes: Routes = [
   { path: 'cosmetics', component: CosmeticsComponent },
   { path: 'decor', component: DecorComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'dash', component: DashComponent, canActivate: [AuthGuard] },
+
 
   { path: '**', component: PageNotFoundComponent },
 ];
